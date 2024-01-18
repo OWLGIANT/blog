f2322f944d86933d801ee561fefd715215c398e1
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-01-18 17:38:24 +0800

    fix:文件处理

--- handel.sh
+++ handel.sh
@@ -2,11 +2,9 @@
 # 配置目标文件夹
 folder_path="CompressFiles"
 
-# 检查文件夹是否存在
 if [ -d "$folder_path" ]; then
   echo "文件夹已经存在"
 else
-  # 如果文件夹不存在，则创建
   mkdir -p "$folder_path"
   echo "文件夹已创建"
 fi
@@ -14,24 +12,25 @@ fi
 current_folder=$(pwd)
 file_count=$(find "$current_folder" -maxdepth 1 -type f -name "*.csv*" | wc -l)
 total_size=$(find "$current_folder" -maxdepth 1 -type f -name "*.csv*" -exec du -csh {} + | tail -n 1 | awk '{print $1}')
-
 echo "文件数: $file_count"
 echo "总资源大小: $total_size"
 
 today_start=$(date -d "$(date +"%Y-%m-%d")" +%s)
 
 for file in *.csv; do
+
   mtime=$(stat -c %Y "$file")
   if [ "$mtime" -ge "$today_start" ]; then
-        echo "文件在今天更新过 $today_start  $mtime"
+        echo "文件在今天更新过 $today_start  $mtime  $file"
   else
-        echo "文件不是今天更新的  $today_start  $mtime"
+        echo "文件是之前更新的  $today_start  $mtime  $file"
         if [ ! -f "${file}.gz" ]; then
           gzip -c "$file" > "$folder_path"/"${file}.gz"
         else
-          mv ${file}.gz "$folder_path"/"${file}.gz"
+          mv "${file}.gz" "$folder_path"/"${file}.gz"
         fi
   fi
+
 done
 
 current_folder=$(pwd)
--- handeldelete.sh
+++ handeldelete.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # 遍历目录中的文件根据target_directory  目录文件已有的删除当前文件夹匹配文件
-echo "删除开始"
 target_directory="CompressFiles"
+
+echo "删除开始"
+
 for file in "$target_directory"/*; do
+
     if [ -f "$file" ]; then
+
       filename=$(basename "$file")
       patternFile="${filename%.gz}"
+      echo    "$patternFile"
       rm  -f  "$patternFile"*
+
     fi
 done
+
+
 echo "删除结束"
