e98364c65f82e93fa6f2daad5a041707db65d1b0
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-01-22 15:29:21 +0800

    fix

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module blog
 go 1.21.5
 
 require (
-	github.com/robfig/cron/v3 v3.0.1
+	github.com/pkg/sftp v1.13.6
 	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/crypto v0.18.0
 )
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
 func main() {
+	filePath := "my_file.txt"
 
-	var a float64 = 0.29
+	// 打开文件
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
-	println(a * 100)
+	// 创建文件锁
+	lock := flock.NewFlock(filePath)
+
+	// 尝试加锁
+	locked, err := lock.TryLock()
+	if err != nil {
+		fmt.Println("Error acquiring lock:", err)
+		return
+	}
+
+	if !locked {
+		fmt.Println("Another process holds the lock.")
+		return
+	}
+
+	fmt.Println("Acquired lock")
+
+	// 模拟业务逻辑
+	time.Sleep(10 * time.Second)
+
+	// 释放锁
+	err = lock.Unlock()
+	if err != nil {
+		fmt.Println("Error releasing lock:", err)
+		return
+	}
+
+	fmt.Println("Released lock")
 }
