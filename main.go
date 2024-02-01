22c145f197d9d1563c295a1ff4f02a6adbab26c8
www <1325384144@qq.com>
2024-02-01 21:00:02 +0800

    fix

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"blog/utils"
 	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	commod := fmt.Sprintf(`cd ~/CompressFiles;./debugStraMkd -v  | grep -o '"BuildTime":"[^"]*"' | awk -F'"' '{print $4}'`)
-	outPut, err := utils.SSHCmd("114.55.134.26", commod)
-	if err != nil {
-		println(err)
-	}
-	fmt.Println("------114.55.134.26----------", outPut)
+
 	//
+}
+
+func CheckLastVersion(ip, strategy, version string) bool {
+	command := fmt.Sprintf(`cd /root/supervisor;./%s -v  | grep -o '"BuildTime":"[^"]*"' | awk -F'"' '{print $4}'`, strategy)
+	outPut, err := utils.SSHCmd(ip, command)
+	if err != nil {
+		logrus.Error(err)
+		return false
+	}
+	if outPut != version {
+		return false
+	}
+	return true
 }
