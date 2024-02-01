7e9401200c20a33ed704aac393bd25826a358563
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-02-01 18:28:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	outPut, err := utils.SSHCmd("114.55.134.26", "cd ~/CompressFiles;./test.sh")
+	commod := fmt.Sprintf(`cd ~/CompressFiles;./debugStraMkd -v  | grep -o '"BuildTime":"[^"]*"' | awk -F'"' '{print $4}'`)
+	outPut, err := utils.SSHCmd("114.55.134.26", commod)
 	if err != nil {
 		println(err)
 	}
 	fmt.Println("------114.55.134.26----------", outPut)
+	//
 }
