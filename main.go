a32e749d397b78ff45bb1df4b87b785483ab9f9a
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-02-20 18:34:29 +0800

    change

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"runtime"
+	"runtime/pprof"
 	"time"
 )
 
@@ -13,17 +16,27 @@ func main() {
 		fmt.Println("http://localhost:6060/debug/pprof")
 		http.ListenAndServe("localhost:6060", nil)
 	}()
-	fmt.Println(time.Now().Format(time.DateTime))
-	tickInfo := time.NewTicker(time.Second)
-	defer tickInfo.Stop()
-	for {
-		select {
-		case <-tickInfo.C:
-			//Write()
-			var data []string
-			data = append(data, "test")
-		}
+	// 手动触发 Heap Dump
+	f, err := os.Create("heapdump.pprof")
+	if err != nil {
+		panic(err)
 	}
+	defer f.Close()
+
+	fmt.Println("===开始=========", time.Now().Format(time.DateTime))
+
+	for i := 0; i < 100000; i++ {
+		var data []string
+		data = append(data, "test")
+	}
+	fmt.Println("===结束=========", time.Now().Format(time.DateTime))
+	runtime.GC() // 执行垃圾回收
+	pprof.WriteHeapProfile(f)
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Hour)
 }
 
 func Write() {
