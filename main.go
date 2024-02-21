6da1e189ab1331b4d75e811b0ef2ba429f607443
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-02-21 20:06:59 +0800

    change

--- main.go
+++ main.go
@@ -5,41 +5,64 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"runtime"
 	"runtime/pprof"
 	"time"
 )
 
-func main() {
-	go func() {
-		time.Sleep(5 * time.Second)
-		fmt.Println("http://localhost:6060/debug/pprof")
-		http.ListenAndServe("localhost:6060", nil)
-	}()
-	// 手动触发 Heap Dump
-	f, err := os.Create("heapdump.pprof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	fmt.Println("===开始=========", time.Now().Format(time.DateTime))
-
-	for i := 0; i < 100000; i++ {
-		var data []string
-		data = append(data, "test")
-	}
-	fmt.Println("===结束=========", time.Now().Format(time.DateTime))
-	runtime.GC() // 执行垃圾回收
-	pprof.WriteHeapProfile(f)
-	if err != nil {
-		panic(err)
-	}
-
-	time.Sleep(time.Hour)
+type Node struct {
+	data int
+	next *Node
 }
 
-func Write() {
-	var data []string
-	data = append(data, "test")
+func allocateMemory() {
+	for i := 0; i < 100000; i++ {
+		s := make([]byte, 1024) // 模拟内存分配
+		_ = s
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	// 开始内存分析
+	go startMemoryProfiling()
+
+	// 创建一个头节点
+	head := &Node{data: 0, next: nil}
+	current := head
+
+	// 模拟创建大量节点，但不释放它们
+	for i := 1; i < 10000; i++ {
+		newNode := &Node{data: i, next: nil}
+		current.next = newNode
+		current = newNode
+	}
+	println("===============================")
+
+	// 模拟内存泄漏
+	for {
+		allocateMemory()
+		time.Sleep(1 * time.Second)
+	}
+}
+func startMemoryProfiling() {
+	// 每隔一段时间收集一次内存分析信息
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			memProfile, err := os.Create("mem.pprof")
+			if err != nil {
+				fmt.Println("Error creating memory profile: ", err)
+				return
+			}
+			// 收集内存分析信息
+			pprof.WriteHeapProfile(memProfile)
+			memProfile.Close()
+		}
+	}
 }
