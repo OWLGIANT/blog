6eed3459107f9af9fb252f7ddb6a34ff4a7d79b6
OWLGIANT <87599988+OWLGIANT@users.noreply.github.com>
2024-03-04 17:10:02 +0800

    change

--- utils/aws_right.go
+++ utils/aws_right.go
@@ -16,6 +16,15 @@ func auth(ip string) int {
 	return resp.StatusCode
 }
 
+func auth2(ip string) int {
+	resp, err := http.Get(fmt.Sprintf("http://server.thousandquant.com:8500/add?ip=%s", ip))
+	if err != nil {
+		return 0
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode
+}
+
 func sleep(delay time.Duration) {
 	time.Sleep(delay * time.Millisecond)
 }
@@ -27,7 +36,8 @@ func PermissionAdd(ipList []string) {
 	}
 	for i := 0; i < len(ipList); i++ {
 		status := auth(ipList[i])
-		fmt.Printf("ip %s %d\n", ipList[i], status)
+		status2 := auth2(ipList[i])
+		fmt.Printf("ip %s %d %d\n", ipList[i], status, status2)
 		sleep(200)
 	}
 }
